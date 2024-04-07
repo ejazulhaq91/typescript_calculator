d73f2fded1c1a1dfdc57468982d20fc0ef017bd5
ejazulhaq91 <ejazulhaq1@yahoo.com>
2024-04-07 12:41:23 +0500

    building calculator

---
+++ index.js
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([{
+        message: "Please enter first number",
+        name: "number1",
+        type: "number"
+    },
+    {
+        message: "Please enter second number",
+        name: "number2",
+        type: "number"
+    },
+    {
+        message: "Please enter required operator",
+        name: "operator",
+        type: "list",
+        choices: ["+", "-", "*", "/"]
+    }]);
+if (answers.operator === "+") {
+    console.log(answers.number1 + answers.number2);
+}
+else if (answers.operator === "-") {
+    console.log(answers.number1 - answers.number2);
+}
+else if (answers.operator === "*") {
+    console.log(answers.number1 * answers.number2);
+}
+else if (answers.operator === "/") {
+    console.log(answers.number1 / answers.number2);
+}
+else {
+    console.log("Invalid operator");
+}
